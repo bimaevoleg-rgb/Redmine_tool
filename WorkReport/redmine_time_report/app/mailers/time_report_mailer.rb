8f0d48422c3c946fe6858c6aaaf6618778db2b1b
bimaevoleg-rgb <bimaevoleg-rgb@users.noreply.github.com>
2026-08-28 17:50:01 +0300

    Исправить From и multipart в mailer отчёта

--- WorkReport/redmine_time_report/app/mailers/time_report_mailer.rb
+++ WorkReport/redmine_time_report/app/mailers/time_report_mailer.rb
@@ -7,8 +7,9 @@ class TimeReportMailer < ActionMailer::Base
       attachments["time_report_#{Date.today.iso8601}.csv"] = { mime_type: 'text/csv', content: report[:csv] }
     end
     mail to: recipients,
-         subject: "Отчёт по трудозатратам #{Date.today.strftime('%d.%m.%Y')}",
-         body: TimeReport.build_html(report),
-         content_type: 'text/html'
+         from: Setting['mail_from'],
+         subject: "Отчёт по трудозатратам #{Date.today.strftime('%d.%m.%Y')}" do |format|
+      format.html { render html: TimeReport.build_html(report).html_safe }
+    end
   end
 end
